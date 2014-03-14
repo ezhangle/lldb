d71560c29753a570aaf0e32460ee3c4e474440c1
Andrew MacPherson <andrew.macp@gmail.com>
2014-03-14 09:49:34 +0100

    Use 0 for invalid line entry data, #2204.

--- include/lldb/lldb-defines.h
+++ include/lldb/lldb-defines.h
@@ -94,7 +94,7 @@
 #define LLDB_INVALID_FRAME_ID           UINT32_MAX
 #define LLDB_INVALID_SIGNAL_NUMBER      INT32_MAX
 #define LLDB_INVALID_OFFSET             UINT64_MAX // Must match max of lldb::offset_t
-#define LLDB_INVALID_LINE_NUMBER        UINT32_MAX
+#define LLDB_INVALID_LINE_NUMBER        0
 #define LLDB_INVALID_QUEUE_ID           0
 
 //----------------------------------------------------------------------
